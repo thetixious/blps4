8725d09ac906c321cc0323f09f6b231745ecaa88
Arkadiy <ri.arkadiy@gmail.com>
2024-10-04 03:52:56 +0300

    minor fix

--- src/main/java/org/tix/lab4/delegate/TimerTask.java
+++ src/main/java/org/tix/lab4/delegate/TimerTask.java
@@ -2,8 +2,9 @@ package org.tix.lab4.delegate;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
 import org.tix.lab4.repo.CreditRepository;
-
+@Component
 public class TimerTask implements JavaDelegate {
 
     private final CreditRepository creditRepository;
